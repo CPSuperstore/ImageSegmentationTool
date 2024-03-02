1ef8f518cec2dab5a13c9bb6abf4d3a75ad5660b
Christopher P. <cpsuperstoreinc@gmail.com>
2024-03-02 12:52:00 -0500

    Adjusted SLIC slider range

--- segmentation_tool/interface/slic.py
+++ segmentation_tool/interface/slic.py
@@ -7,7 +7,7 @@ class SLIC(segmentation_if.SegmentationInterface):
     def _get_controls(self):
         return [
             segmentation_if.Control("Segments", "number", "n_segments", default=100, allowed_values=[100, 1000]),
-            segmentation_if.Control("Compactness", "number", "compactness", default=1, allowed_values=[0, 100], step=0.01),
+            segmentation_if.Control("Compactness", "number", "compactness", default=1, allowed_values=[0, 10], step=0.01),
             segmentation_if.Control("Max Iterations", "number", "max_num_iter", default=1, allowed_values=[1, 100]),
             segmentation_if.Control("Sigma", "number", "sigma", default=1, allowed_values=[0, 100], step=0.01),
             segmentation_if.Control("Min Size Factor", "number", "min_size_factor", default=1, allowed_values=[1, 10], step=0.01),
